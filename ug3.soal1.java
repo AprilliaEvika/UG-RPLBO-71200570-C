3deaaa113dc15cfa9d93fd81be1d7ee0966bbf4a
AprilliaEvika <78426454+AprilliaEvika@users.noreply.github.com>
2022-02-23 18:17:35 +0700

    Add files via upload

---
+++ ug3.soal1.java
@@ -0,0 +1,21 @@
+package src.main.java.com.ug3.soal1;
+
+public class Main {
+
+    public static void main(String[]) args) {
+        // writer your code here
+        int inputan;
+        System.out.println("Masukkan Data Product Makanan Anda");
+         = ();
+        Nama makanan = ();
+        Tanggal kadaluarsa = ();
+        Jumlah (quantity)= ();
+        Berat (gram)= ();
+        System.out.println(Nama makanan);
+        System.out.println(Tanggal kadaluarsa);
+        System.out.println(Jumlah (quantity));
+        System.out.println(Berat());
+
+
+    }
+}
