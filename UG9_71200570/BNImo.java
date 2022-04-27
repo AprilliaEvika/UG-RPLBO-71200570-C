1225259af2a9271387b6d8e7690f6e2942768e6b
AprilliaEvika <78426454+AprilliaEvika@users.noreply.github.com>
2022-04-27 18:53:37 +0700

    Add files via upload

--- UG9_71200570/BNImo.java
+++ UG9_71200570/BNImo.java
@@ -2,6 +2,8 @@ package com.ug9.etransactionproject;
 
 
 public class BNImo extends com.ug9.etransactionproject.MobileBanking {
-    public BNImo(String, long, String);
+    public BNImo(String, long, String){
+        this.nama = nama;
+    }
     public void transfer(com.ug9.eTransactionProject.DigitalPayment, long)
 }
